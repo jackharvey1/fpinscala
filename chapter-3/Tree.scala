7c71ab1a98b8611593b2d5fcf319dde11ab254b0
Jack Harvey <jackrrv@gmail.com>
2020-09-08 14:45:54 +0100

    Exercise 3.29: size, maximum & depth with fold

--- chapter-3/Tree.scala
+++ chapter-3/Tree.scala
@@ -23,23 +23,36 @@ object Tree {
     case Leaf(x) => Leaf(f(x))
   }
 
+  def fold[A,B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = tree match {
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    case Leaf(x) => f(x) 
+  }
+
+  def size2[A](tree: Tree[A]): Int = fold(tree)(_ => 1)(1 + _ + _)
+
+  def maximum2(tree: Tree[Int]): Int = fold(tree)(a => a)(_ max _)
+
+  def depth2[A](tree: Tree[A]): Int = fold(tree)(_ => 1)((d1, d2) => 1 + (d1 max d2))
+
+  def map2[A,B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
+
   def main(args: Array[String]): Unit = {
-    println(
-      map(
+    val x = Branch(
+      Branch(
+        Leaf(9), 
         Branch(
-          Branch(
-            Leaf(9), 
-            Branch(
-              Leaf(11),
-              Leaf(65) // 4 depth
-            ), 
-          ),
-          Branch(
-            Leaf(2), 
-            Leaf(4)
-          )
-        )
-      )(_ * 2)
+          Leaf(11),
+          Leaf(65) 
+        ), 
+      ),
+      Branch(
+        Leaf(2), 
+        Leaf(4)
+      )
     )
+    println(size2(x))
+    println(maximum2(x))
+    println(depth2(x))
+    println(map2(x)(_ * 2))
   }
 }
