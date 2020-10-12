809d5c55fee52c12e284dcfd3ccdc327af1828c2
Jack Harvey <jackrrv@gmail.com>
2020-10-12 16:33:35 +0100

    add Option set up

---
+++ chapter-4/Option.scala
@@ -0,0 +1,10 @@
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+trait Option[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => Option[B]): Option[B]
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
